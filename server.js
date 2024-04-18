9469822809b15123a2ef522764d4993f74611c08
finntendoverse <finnlambouris616@gmail.com>
2024-04-18 18:52:55 -0400

    added code comments

--- server.js
+++ server.js
@@ -12,72 +12,76 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // routes
-app.get('/notes', (req, res) => {
-  return res.sendFile(path.join(__dirname, './public/notes.html'));
+app.get('/notes', (req, res) => {                                     // WHEN the GET request is called at /notes
+  return res.sendFile(path.join(__dirname, './public/notes.html'));   // THEN the notes.html page will be displayed
 });
 
-app.get('/api/notes', (req, res) => {
-  return res.sendFile(path.join(__dirname, './db/db.json'));
+app.get('/api/notes', (req, res) => {                                 // WHEN the GET request is called at /api/notes
+  return res.sendFile(path.join(__dirname, './db/db.json'));          // THEN the notes database will be displayed
 });
 
-app.post('/api/notes', (req, res) => {
-  const { title, text } = req.body;
+app.post('/api/notes', (req, res) => {                                                        // WHEN the POST request is called at /api/notes
+  const { title, text } = req.body;                                                           // THEN the information within the requests body is destructured
 
-  if (title && text) {
-    fs.readFile(path.join(__dirname, './db/db.json'), 'utf-8', (err, data) => {
-      if(err) {
-        console.log('There was trouble reaching your notes. Please try again.')
-      } else {
-        let notes = JSON.parse(data);
+  if (title && text) {                                                                        // IF the request contains a note title and note text
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf-8', (err, data) => {         // THEN the notes database will be read
+      if(err) {                                                                                 // IF there is an error reading the file
+        console.log('There was trouble reaching your notes. Please try again.')                 // THEN the user will be prompted to try again
+      } else {                                                                                  // IF there are no errors
+        let notes = JSON.parse(data);                                                           // THEN the data within the database file is parsed into an array of objects called "notes"
 
         // Variable for the object we will save
-        const newNote = {
-          title: title,
-          text: text,
-          id: uuid.v1()
+        const newNote = {                                                                       // THEN a new note is created with the request body info
+          title: title,                                                                         // THEN the title is set to the user's title input
+          text: text,                                                                           // THEN the text is set to the users text input
+          id: uuid.v1()                                                                         // THEN uuid will create a unique item for the note
         };
 
-        notes.push(newNote);
+        notes.push(newNote);                                                                    // THEN the new note is added to the notes array
 
-        fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes), (err) => {
-          err ? console.log('there was an error adding the note') : console.log('A new note has been added!');
+        fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes), (err) => {    // THEN the database file is written to
+          if (err) {                                                                            // IF there is an error
+            console.log('there was an error adding the note');                                  // THEN the user will be alerted that there was an error
+          } else {                                                                              // IF there are no errors
+            console.log('A new note has been added!');                                          // THEN the user will be alerted that a note was added
+          }  
         });
     
-        return res.json(newNote);
+        return res.json(newNote);                                                               // THEN the response is returned
       };
     });
-  } else {
-    console.log('Please enter a note title and description.');
+  } else {                                                                                    // IF the request does not contain a note title and note text
+    console.log('Please enter a note title and description.');                                // THEN the user will be asked to enter a title and description
   };
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = req.params.id;
-
-  fs.readFile(path.join(__dirname, './db/db.json'), 'utf-8', (err, data) => {
-    if (err) {
-      console.log('There was trouble reaching your notes. Please try again.')
-    } else {
-      let notes = JSON.parse(data);
-      console.log(req.params.id);
-
-      for (i = 0; i < notes.length; i++) {
-        if (notes[i].id === req.params.id) {
-          notes.splice(i, 1);
+app.delete('/api/notes/:id', (req, res) => {                                                  // WHEN the DELETE request is called on the .api/notes/:id path
+  fs.readFile(path.join(__dirname, './db/db.json'), 'utf-8', (err, data) => {                 // THEN the database file is read
+    if (err) {                                                                                // IF there is an error
+      console.log('There was trouble reaching your notes. Please try again.')                 // THEN the user will be alerted to try again
+    } else {                                                                                  // IF there are no errors
+      let notes = JSON.parse(data);                                                           // THEN the data within the database file is parsed into an array of objects called "notes"
+      for (i = 0; i < notes.length; i++) {                                                    // FOR each note in the array
+        if (notes[i].id === req.params.id) {                                                      // IF the note id matches the id if the delete request
+          removedNote = notes.splice(i, 1);                                                       // THEN the note is removed from the array
         }
       }
 
-      fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes), (err) => {
-        err ? console.log('there was an error removing the note') : console.log('A note has been removed!');
+      fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes), (err) => {    // THEN the file is written with all the notes
+        if (err) {                                                                                // IF there is an error
+          console.log('there was an error removing the note');                                    // THEN the user is alerted that there was an error
+        } else {                                                                                  // IF there are no errors
+          console.log('A note has been removed!');                                                // THEN the user is alerted that a note was deleted
+        }
       });
 
-      return res.json(notes);
+      return res.json(removedNote);                                                               // THEN the removed note is returned
     }
   });
 });
 
-app.get('*', (req, res) => {
-  return res.sendFile(path.join(__dirname, './public/index.html'))
+app.get('*', (req, res) => {                                            // WHEN the GET requesr is called is called on anything other than our specified paths
+  return res.sendFile(path.join(__dirname, './public/index.html'))      // THEN the homepage is returned
 });
 
 // app listener
