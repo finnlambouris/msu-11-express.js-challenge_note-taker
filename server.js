4a51e81a84c2d1bcc9f6a7270dcf8f7311be00c4
finntendoverse <finnlambouris616@gmail.com>
2024-04-18 14:10:52 -0400

    added post path

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // required modules
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const uuid = require('uuid');
 
 // creating the app
 const app = express();
@@ -11,19 +13,44 @@ app.use(express.static('public'));
 
 // routes
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+  return res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './db/db.json'));
+  return res.sendFile(path.join(__dirname, './db/db.json'));
 });
 
-// app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
 
-// });
+  if (title && text) {
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf-8', (err, data) => {
+      err ? console.log('trouble reading the file') : console.log(data);
+      
+      let notes = JSON.parse(data);
+
+      // Variable for the object we will save
+      const newNote = {
+        title: title,
+        text: text,
+        id: uuid.v1()
+      };
+
+      notes.push(newNote);
+
+      fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes), (err) => {
+        err ? console.log('there was an error adding the note') : console.log('A new note has been added!');
+      });
+  
+      return res.json(newNote);
+    })
+  } else {
+    return res.send(err);
+  }
+});
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'))
+  return res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
 // app listener
